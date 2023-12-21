e6d30c94587e7855e9bbce9461f6df8385d327db
sarvari1378 <137143835+sarvari1378@users.noreply.github.com>
2023-12-21 17:12:30 +0330

    Update CreateV2ray.py

--- Scripts/CreateV2ray.py
+++ Scripts/CreateV2ray.py
@@ -4,23 +4,42 @@ import requests
 import regex
 from collections import namedtuple
 import os
-import jdatetime
+import base64
 
+#################### start of Functions
 def get_config(urls):
+    responses = {}
     for url in urls:
         if url:  # Check if the URL is not empty
             try:
                 response = requests.get(url)
                 if response.status_code == 200:
-                    return response.text  # get the raw content of the response
+                    responses[url] = response.text  # get the raw content of the response
             except requests.exceptions.RequestException as e:
                 print(f"An error occurred: {e}")
-    return None
+    return responses
 
+####################
+def merge_content(responses):
+    merged_content = ''
+    for url in responses:
+        content = responses[url]
+        if content is not None:
+            try:
+                decoded_content = base64.b64decode(content).decode()
+                merged_content += decoded_content + '\n'
+            except Exception:
+                # Content is not base64 encoded, add as it is
+                merged_content += content + '\n'
+    return merged_content
+
+
+####################
 def extract_flag(line):
     match = regex.search(r'\p{So}\p{So}', line)
     flag = match.group() if match else ''
     return flag
+####################
 
 def rename_configs(content, name):
     lines = content.split('\n')
@@ -34,22 +53,25 @@ def rename_configs(content, name):
             line = line.split('#')[0]
             
             now = datetime.now(pytz.timezone('Asia/Tehran'))
-            hour = now.strftime('%H:%M')
-            date = jdatetime.date.today().strftime('%Y-%m-%d')
+            hour = now.strftime('%H:%M:%S')
+            date = now.strftime('%Y-%m-%d')
             # Add flag to new name
             line += f'#|{flag}|{hour}|{date}|{name}|{i+1}|'
         new_lines.append(line)
     return '\n'.join(new_lines)
+####################
 
 def save_to_file(content, filename):
     with open(filename, 'w') as f:
         f.write(content)
+####################
 
 def remove_lines(content, num):
     lines = content.split('\n')
     # Remove the first 'num' lines
     del lines[:num]
     return '\n'.join(lines)
+####################
 
 def get_users(url):
     response = requests.get(url)
@@ -65,7 +87,9 @@ def get_users(url):
 
     return users
 
-def Create_SUBs(users, urls,PROCTCOLE):
+####################
+
+def Create_SUBs(users, responses, PROTOCOL):
     # Create 'SUB' directory if it doesn't exist
     if not os.path.exists('SUB'):
         os.makedirs('SUB')
@@ -73,25 +97,25 @@ def Create_SUBs(users, urls,PROCTCOLE):
     for user in users:
         # Check if user's date as a number is zero or less
         if float(user.date) <= 0:
-            content = 'vless://64694d4a-2c05-4ffe-aef1-68c0169cccb7@146.248.115.39:443?encryption=none&fp=firefox&mode=gun&pbk=TXpA-KUEqsg6YlZUXf0gZIe14rFjKZZNAqWzjruNoh8&security=reality&serviceName=&sid=790d3c76&sni=www.speedtest.net&spx=%2F&type=grpc#Your-subscription-has-ended'
+            content = 'vless://64694D4A-2C05-4FFE-AEF1-68C0169CCCB7@146.248.115.39:443?encryption=none&fp=firefox&mode=gun&pbk=TXpA-KUEqsg6YlZUXf0gZIe14rFjKZZNAqWzjruNoh8&security=reality&serviceName=&sid=790D3C76&sni=www.speedtest.net&spx=%2F&type=grpc#Your-subscription-has-ended'
         else:
-            content = get_config(urls)
-            content = remove_lines(content,6)
-            if content is not None:
-                content = rename_configs(content, user.username)
-                line = f'vless://64694d4a-2c05-4ffe-aef1-68c0169cccb7@146.248.115.39:443?encryption=none&fp=firefox&mode=gun&pbk=TXpA-KUEqsg6YlZUXf0gZIe14rFjKZZNAqWzjruNoh8&security=reality&serviceName=&sid=790d3c76&sni=www.speedtest.net&spx=%2F&type=grpc#|👤User: {user.username}|⌛️Remain Days: {user.date}|'
-                content = line + '\n' + content
+            merged_content = merge_content(responses)
+            content = rename_configs(merged_content, user.username)
+            line = f'vless://64694D4A-2C05-4FFE-AEF1-68C0169CCCB7@146.248.115.39:443?encryption=none&fp=firefox&mode=gun&pbk=TXpA-KUEqsg6YlZUXf0gZIe14rFjKZZNAqWzjruNoh8&security=reality&serviceName=&sid=790D3C76&sni=www.speedtest.net&spx=%2F&type=grpc#|👤User: {user.username}|⌛️Remain Days: {user.date}|'
+            content = line + '\n' + content
 
-        filename = f'SUB/{PROCTCOLE}-{user.username}'
+        filename = f'SUB/{PROTOCOL}-{user.username}'
         with open(filename, 'w') as f:
             f.write(content)
 
+########################### end of functions
 
-url = 'https://raw.githubusercontent.com/sarvari1378/GPTscripts/main/Users.txt'
-users = get_users(url)
+User_url = 'https://raw.githubusercontent.com/sarvari1378/SingBOX/main/Users.txt'
+
+users = get_users(User_url)
 urls = [
-    "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/normal/reality",
-    "https://raw.githubusercontent.com/yebekhe/TelegramV2rayCollector/main/sub/normal/tuic"
+    "https://nv2ron.ir/subscription.link.QV2RAY?NTYxNTc2Mjk3MS0xNzAyOTcwMzI0"
 ]
 
-Create_SUBs(users, urls,'REALITY')
+responses = get_config(urls)
+Create_SUBs(users, responses, 'REALITY')
