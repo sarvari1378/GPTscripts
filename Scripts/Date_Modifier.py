bba3052c2a5df75b702de8724cd5fa6ae37e2c61
sarvari1378 <137143835+sarvari1378@users.noreply.github.com>
2024-03-17 13:30:33 +0330

    Update Date_Modifier.py

--- Scripts/Date_Modifier.py
+++ Scripts/Date_Modifier.py
@@ -1,4 +1,4 @@
-import requests
+import uuid
 
 class User:
     def __init__(self, name, date, tel_username=None):
@@ -6,34 +6,35 @@ class User:
         self.date = date
         self.tel_username = tel_username
 
-def read_users_from_url(url):
+def read_users_from_file(filename):
     users = []
-    response = requests.get(url)
-    lines = response.text.split('\n')
-    for line in lines:
-        parts = line.strip().split(', ')
-        if parts[0]:  # check if line is not empty
-            name = parts[0]
-            date = int(parts[1])
-            tel_username = parts[2] if len(parts) > 2 else None
-            user = User(name, date, tel_username)
-            users.append(user)
+    with open(filename, 'r') as file:
+        lines = file.readlines()[1:]  # Skip the first line
+        for line in lines:
+            parts = line.strip().split(', ')
+            if parts[0]:  # check if line is not empty
+                name = parts[0]
+                date = int(parts[1])
+                tel_username = parts[2] if len(parts) > 2 else None
+                user = User(name, date, tel_username)
+                users.append(user)
     return users
-
 
 def write_users_to_file(users, filename):
     with open(filename, 'w') as file:
+        unique_id = str(uuid.uuid4())[:6]  # Generate a unique identifier
+        file.write(f"{unique_id}, 1\n")  # Write the new unique identifier at the beginning of the file
         for user in users:
             line = f"{user.name}, {user.date}"
             if user.tel_username:
                 line += f", {user.tel_username}"
             file.write(line + '\n')
 
-
 # Example of usage
-users = read_users_from_url('https://raw.githubusercontent.com/sarvari1378/GPTscripts/main/Users.txt')
+users_file = 'Users.txt'
+users = read_users_from_file(users_file)
 
 for user in users:
     user.date = user.date - 1
-   
-write_users_to_file(users,'Users.txt')
+
+write_users_to_file(users, users_file)
